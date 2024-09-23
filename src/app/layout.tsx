b0b2a93031b74a7cbce8ae10d3463003503265c2
henry <pye.henry@protonmail.com>
2024-09-22 18:47:25 -0700

    Move all PWA manifest into app metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,37 +1,37 @@
-import type { Metadata } from "next";
-import Head from "next/head";
-import { NEXT_PUBLIC_URL } from "../config";
-
 import "@coinbase/onchainkit/styles.css";
 import "@rainbow-me/rainbowkit/styles.css";
+import type { Metadata } from "next";
 import dynamic from "next/dynamic";
+import { NEXT_PUBLIC_URL } from "../config";
 import "./global.css";
 
+// Dynamically import providers to disable server-side rendering (ssr: false)
 const OnchainProviders = dynamic(() => import("src/components/OnchainProviders"), {
   ssr: false,
 });
 
-export const viewport = {
-  width: "device-width",
-  initialScale: 1.0,
-};
-
+// Metadata object handling SEO, openGraph, and PWA manifest
 export const metadata: Metadata = {
-  title: "Onchain App Template",
-  description: "Built with OnchainKit",
+  title: "TRASH - Beta",
+  description: "A Tokenized Circular Economy for Sustainable Fashion.",
   openGraph: {
-    title: "Onchain App Template",
-    description: "Built with OnchainKit",
+    title: "TRASH - Beta",
+    description: "A Tokenized Circular Economy for Sustainable Fashion.",
     images: [`${NEXT_PUBLIC_URL}/vibes/vibes-19.png`],
   },
+  viewport: {
+    width: "device-width",
+    initialScale: 1.0,
+  },
+  icons: {
+    icon: "/favicon.ico", // Optional: specify icons if needed
+  },
+  manifest: "/manifest.json", // Link to your PWA manifest
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <Head>
-        <link rel="manifest" href="/manifest.json"></link>
-      </Head>
       <body className="flex items-center justify-center">
         <OnchainProviders>{children}</OnchainProviders>
       </body>
